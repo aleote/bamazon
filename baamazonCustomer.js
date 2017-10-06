4901ffcca26394e07dcc496bdd77e7f958acb1ea
annalisa leote <amleote@gmail.com>
2017-10-06 17:08:49 -0400

    final error eliminated

--- baamazonCustomer.js
+++ baamazonCustomer.js
@@ -2,86 +2,86 @@ var mysql = require("mysql");
 var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
+    host: "localhost",
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "",
-  database: "bamazonDB"
+    // Your password
+    password: "",
+    database: "bamazonDB"
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  afterConnection();
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    afterConnection();
 });
 
 function afterConnection() {
-  connection.query("SELECT * FROM products", function(err, res) {
-    if (err) {
-    throw err; 
-  } else {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) {
+            throw err;
+        } else {
 
-    for (var i = 0; i < res.length; i++) {
-    
-    console.log("------------------------------------------------------");
-    console.log("Item Id:" + res[i].item_id);
-    console.log("Product Name:" + res[i].product_name);
-    console.log("Department" + res[i].department_name);
-    console.log("Price:" + res[i].price);
-    console.log("Quanitity:" + res[i].stock_quantity);
-    console.log("------------------------------------------------------");
+            for (var i = 0; i < res.length; i++) {
 
-    var currentQty = res[i].stock_quantity;
-  
+                console.log("------------------------------------------------------");
+                console.log("Item Id:" + res[i].item_id);
+                console.log("Product Name:" + res[i].product_name);
+                console.log("Department" + res[i].department_name);
+                console.log("Price:" + res[i].price);
+                console.log("Quanitity:" + res[i].stock_quantity);
+                console.log("------------------------------------------------------");
+
+                var currentQty = res[i].stock_quantity;
+
+            }
+            start(currentQty);
+        }
+    });
 }
-    start(currentQty);
-  }
-  });
-}
-
+var order;
 
 function start(current) {
-  console.log("THIS IS CURRENT" + current);
-  inquirer.prompt([
-    {
-      name: "idSearch",
-      type: "input",
-      message: "What is the id of the item that you would like to find?"
-     
-    },
-    {
-      name: "quantityPrompt",
-      type: "input",
-      message:"How many would you like to purchase?"
-    }
-    ]).then(function(response) {
+    // console.log("THIS IS CURRENT" + current);
+    inquirer.prompt([{
+        name: "idSearch",
+        type: "input",
+        message: "What is the id of the item that you would like to find?"
 
-      var orderId = parseInt(response.idSearch);
-      var orderIndex = orderId -1;
-      var orderQty = parseInt(response.quantityPrompt);
+    }, {
+        name: "quantityPrompt",
+        type: "input",
+        message: "How many would you like to purchase?"
+    }]).then(function(response) {
 
-      connection.query('SELECT * FROM products', function(err, res){
-        var currentQty = res[orderIndex].stock_quantity;
+        var orderId = parseInt(response.idSearch);
+        var orderIndex = orderId;
+        var orderQty = parseInt(response.quantityPrompt);
+        order = orderQty;
 
-      })
-       console.log(typeof current);
-        if(current > response.quantityPrompt){
-          console.log("working");
-          var qtyOrdered = response.quantityPrompt;
-          var idOrdered = orderIndex;
-          completeOrder(idOrdered, qtyOrdered, current);  
+        connection.query('SELECT * FROM products', function(err, res) {
+                var currentQty = res[orderIndex].stock_quantity;
+
+            })
+            // console.log(typeof current);
+            console.log("current: ", current);
+            console.log("quantityPrompt: ", response.quantityPrompt);
+        if (current > response.quantityPrompt) {
+
+           
+            var qtyOrdered = response.quantityPrompt;
+            var idOrdered = orderIndex;
+            completeOrder(idOrdered, qtyOrdered, current);
+        } else {
+            console.log("Insufficient Quantity!");
+            start();
+
+
         }
-      else {
-        console.log("Insufficient Quantity!");
-        start();
-
-
-      }
-});
+    });
 
 
 }
@@ -90,47 +90,55 @@ function start(current) {
 
 function completeOrder(id, stock, current) {
 
-  var newQTY = parseInt(current) - stock;
-  console.log("Updated QTY: " + newQTY);
+    var newQTY = parseInt(current) - stock;
+    console.log("Updated QTY: " + newQTY);
 
-    connection.query("UPDATE PRODUCTS SET ? WHERE ?",[
-      {
-        stock_quantity: newQTY
-      },
-      {
-        item_id: id
-      }
-      ],
-       function(err, res) {
-    if (err) {
-    throw err; 
-    showCost(id, order)
- } 
+    connection.query("UPDATE PRODUCTS SET ? WHERE ?", [{
+            stock_quantity: newQTY
+        }, {
+            item_id: id
+        }],
+        function(err, res) {
+            if (err) {
+                throw err;
+            }
+           
 
-});
+            
+            
+       
+            showCost(id, order);
+        });
+}
 
-  }
 
+function showCost(id, order) {
 
-function showCost(id, qty){
-  console.log("Price of Product:" + res[0].price);
-  console.log("Quantity Ordered:" + qty);
-  var orderCost = parseInt(res[0].price) * qty;
-  console.log("Your total cost: $" + orderCost + '\n Thanks for Shopping!');
  
- connection.end();
+    connection.query('SELECT * FROM products WHERE ?', {
+            item_id: id
+        },
+        function(err, res) {
+            
+            console.log("Price of Product:" + res[0].price);
+            console.log("Quantity Ordered:" + order);
+            var orderCost = parseInt(res[0].price) * order;
+            console.log("Your total cost: $" + orderCost + '\n Thanks for Shopping!');
 
+            connection.end();
+
+        });
 }
 
 
 
 
 
- // //query SELECT, find current stock of item ordered
- //      connection.query("SELECT * FROM currentQty")
- //      //parseInt() current stock and qty of stock ordered
- //      parseInt(currentQty, quantitiyPrompt)
- //      //find difference to get new stock... = newStock
- //      var NewStock = currentQty - quantityPrompt
- //      //pass new stock into completeOrder() with id
- //      //UPDATE in completeOrder() stock_quantity: newStock}
+// //query SELECT, find current stock of item ordered
+//      connection.query("SELECT * FROM currentQty")
+//      //parseInt() current stock and qty of stock ordered
+//      parseInt(currentQty, quantitiyPrompt)
+//      //find difference to get new stock... = newStock
+//      var NewStock = currentQty - quantityPrompt
+//      //pass new stock into completeOrder() with id
+//      //UPDATE in completeOrder() stock_quantity: newStock}
